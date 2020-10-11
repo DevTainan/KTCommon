60d565f8e21110d81d966ae469e8297350d31d2d
kentseng <kentseng@fs-technology.com>
2020-10-11 13:32:59 +0800

    1. 補上 log 功能

---
+++ KTCommon/KTCommon/Log/IFileLog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KTCommon
+{
+    /// <summary>
+    /// log 檔案寫入的介面
+    /// </summary>
+    public interface IFileLog
+    {
+        /// <summary>
+        /// 寫入 log
+        /// </summary>
+        /// <param name="strMessage"></param>
+        /// <param name="errorCode"></param>
+        void Log(string strMessage, LogLevelType errorCode);
+
+        /// <summary>
+        /// 寫入 log
+        /// </summary>
+        /// <param name="strTime"></param>
+        /// <param name="strMessage"></param>
+        /// <param name="errorCode"></param>
+        void Log(string strTime, string strMessage, LogLevelType errorCode);
+    }
+}
